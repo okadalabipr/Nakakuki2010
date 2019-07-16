8b8f19d53be914fffec5a36d2d678af779c61811
himoto <himoto@protein.osaka-u.ac.jp>
2019-07-16 18:12:01 +0900

    Update plot_func.py

--- plot_func.py
+++ plot_func.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
 
 def timecourse(sim):
     plt.figure(figsize=(20,8))
     plt.rcParams['font.size'] = 16
     plt.rcParams['font.family'] = 'Arial'
+    plt.rcParams['mathtext.fontset'] = 'custom'
+    plt.rcParams['mathtext.it'] = 'Arial:italic'
     plt.rcParams['axes.linewidth'] = 2
     plt.rcParams['lines.linewidth'] = 2.5
     plt.rcParams['lines.markersize'] = 10
@@ -12,8 +15,15 @@ def timecourse(sim):
     plt.subplots_adjust(wspace=0.5, hspace=0.5)
 
     plt.subplot(2,4,1)
-    plt.plot(sim.t,sim.PMEK_cyt[:,0],'b',label='EGF')
-    plt.plot(sim.t,sim.PMEK_cyt[:,1],'r',label='HRG')
+    plt.plot(sim.t,sim.PMEK_cyt[:,0],'b',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],label='EGF'
+    )
+    plt.plot(sim.t,sim.PMEK_cyt[:,1],'r',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],label='HRG',
+        clip_on=False
+    )
     plt.xlim(0,90)
     plt.xticks([0,30,60,90])
     plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
@@ -23,8 +33,14 @@ def timecourse(sim):
     plt.legend(loc='upper right',fontsize=12,frameon=False)
 
     plt.subplot(2,4,2)
-    plt.plot(sim.t,sim.PERK_cyt[:,0]/np.max(sim.PERK_cyt[:,1]),'b')
-    plt.plot(sim.t,sim.PERK_cyt[:,1]/np.max(sim.PERK_cyt[:,1]),'r')
+    plt.plot(sim.t,sim.PERK_cyt[:,0]/np.max(sim.PERK_cyt[:,1]),'b',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
+    plt.plot(sim.t,sim.PERK_cyt[:,1]/np.max(sim.PERK_cyt[:,1]),'r',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
     plt.xlim(0,90)
     plt.xticks([0,30,60,90])
     plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
@@ -33,8 +49,14 @@ def timecourse(sim):
     plt.ylabel('Phosphorylated ERK\n(cytoplasm)')
 
     plt.subplot(2,4,3)
-    plt.plot(sim.t,sim.PRSK_wcl[:,0]/np.max(sim.PRSK_wcl[:,1]),'b')
-    plt.plot(sim.t,sim.PRSK_wcl[:,1]/np.max(sim.PRSK_wcl[:,1]),'r')
+    plt.plot(sim.t,sim.PRSK_wcl[:,0]/np.max(sim.PRSK_wcl[:,1]),'b',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
+    plt.plot(sim.t,sim.PRSK_wcl[:,1]/np.max(sim.PRSK_wcl[:,1]),'r',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
     plt.xlim(0,90)
     plt.xticks([0,30,60,90])
     plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
@@ -43,8 +65,14 @@ def timecourse(sim):
     plt.ylabel('Phosphorylated RSK\n(whole cell)')
 
     plt.subplot(2,4,4)
-    plt.plot(sim.t,sim.PCREB_wcl[:,0]/np.max(sim.PCREB_wcl[:,1]),'b')
-    plt.plot(sim.t,sim.PCREB_wcl[:,1]/np.max(sim.PCREB_wcl[:,1]),'r')
+    plt.plot(sim.t,sim.PCREB_wcl[:,0]/np.max(sim.PCREB_wcl[:,1]),'b',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
+    plt.plot(sim.t,sim.PCREB_wcl[:,1]/np.max(sim.PCREB_wcl[:,1]),'r',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
     plt.xlim(0,90)
     plt.xticks([0,30,60,90])
     plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
@@ -53,8 +81,14 @@ def timecourse(sim):
     plt.ylabel('Phosphorylated CREB\n(whole cell)')
 
     plt.subplot(2,4,5)
-    plt.plot(sim.t,sim.DUSPmRNA[:,0]/np.max(sim.DUSPmRNA[:,1]),'b')
-    plt.plot(sim.t,sim.DUSPmRNA[:,1]/np.max(sim.DUSPmRNA[:,1]),'r')
+    plt.plot(sim.t,sim.DUSPmRNA[:,0]/np.max(sim.DUSPmRNA[:,1]),'b',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
+    plt.plot(sim.t,sim.DUSPmRNA[:,1]/np.max(sim.DUSPmRNA[:,1]),'r',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
     plt.xlim(0,90)
     plt.xticks([0,30,60,90])
     plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
@@ -63,8 +97,14 @@ def timecourse(sim):
     plt.ylabel(r'$\it{dusp}$'+' mRNA\nexpression')
 
     plt.subplot(2,4,6)
-    plt.plot(sim.t,sim.cFosmRNA[:,0]/np.max(sim.cFosmRNA[:,1]),'b')
-    plt.plot(sim.t,sim.cFosmRNA[:,1]/np.max(sim.cFosmRNA[:,1]),'r')
+    plt.plot(sim.t,sim.cFosmRNA[:,0]/np.max(sim.cFosmRNA[:,1]),'b',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
+    plt.plot(sim.t,sim.cFosmRNA[:,1]/np.max(sim.cFosmRNA[:,1]),'r',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
     plt.xlim(0,90)
     plt.xticks([0,30,60,90])
     plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
@@ -73,8 +113,14 @@ def timecourse(sim):
     plt.ylabel(r'$\it{c}$'+'-'+r'$\it{fos}$'+' mRNA\nexpression')
 
     plt.subplot(2,4,7)
-    plt.plot(sim.t,sim.cFosPro[:,0]/np.max(sim.cFosPro[:,1]),'b')
-    plt.plot(sim.t,sim.cFosPro[:,1]/np.max(sim.cFosPro[:,1]),'r')
+    plt.plot(sim.t,sim.cFosPro[:,0]/np.max(sim.cFosPro[:,1]),'b',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
+    plt.plot(sim.t,sim.cFosPro[:,1]/np.max(sim.cFosPro[:,1]),'r',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
     plt.xlim(0,90)
     plt.xticks([0,30,60,90])
     plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
@@ -83,8 +129,14 @@ def timecourse(sim):
     plt.ylabel('c-Fos Protein\nexpression')
 
     plt.subplot(2,4,8)
-    plt.plot(sim.t,sim.PcFos[:,0]/np.max(sim.PcFos[:,1]),'b')
-    plt.plot(sim.t,sim.PcFos[:,1]/np.max(sim.PcFos[:,1]),'r')
+    plt.plot(sim.t,sim.PcFos[:,0]/np.max(sim.PcFos[:,1]),'b',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
+    plt.plot(sim.t,sim.PcFos[:,1]/np.max(sim.PcFos[:,1]),'r',
+        path_effects=[path_effects.SimpleLineShadow(),
+        path_effects.Normal()],clip_on=False
+    )
     plt.xlim(0,90)
     plt.xticks([0,30,60,90])
     plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
@@ -92,4 +144,5 @@ def timecourse(sim):
     plt.xlabel('Time (min)')
     plt.ylabel('Phosphorylated c-Fos\nProtein expression')
 
-    plt.show()
+    #plt.show()
+    plt.savefig('./cFosmodel.png',dpi=1200,bbox_inches='tight')
