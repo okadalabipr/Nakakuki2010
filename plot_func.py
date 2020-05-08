4c0f7201e3b48a32fb3fe50455a04b2f2c3fad50
himoto <himoto@protein.osaka-u.ac.jp>
2020-05-08 09:37:08 +0900

    Update plot_func

--- plot_func.py
+++ plot_func.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patheffects as path_effects
+
 
 def timecourse(sim):
-    plt.figure(figsize=(20,8))
+    plt.figure(figsize=(20, 8))
+    # rcParams
     plt.rcParams['font.size'] = 16
     plt.rcParams['axes.linewidth'] = 2
     plt.rcParams['xtick.major.width'] = 2
@@ -17,134 +18,133 @@ def timecourse(sim):
 
     plt.subplots_adjust(wspace=0.5, hspace=0.5)
 
-    plt.subplot(2,4,1)
-    plt.plot(sim.t,sim.PMEK_cyt[:,0],'b',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],label='EGF'
+    plt.subplot(2, 4, 1)  # ----------------------------------------------------
+    plt.plot(
+        sim.t, sim.PMEK_cyt[:, sim.conditions.index('EGF')], 
+        'b', label='EGF', clip_on=False
     )
-    plt.plot(sim.t,sim.PMEK_cyt[:,1],'r',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],label='HRG',
-        clip_on=False
+    plt.plot(
+        sim.t, sim.PMEK_cyt[:, sim.conditions.index('HRG')], 
+        'r', label='HRG', clip_on=False
     )
-    plt.xlim(0,90)
-    plt.xticks([0,30,60,90])
-    plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
-    plt.ylim(0,1.2)
+    plt.xlim(0, 90)
+    plt.xticks([0, 30, 60, 90])
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2])
+    plt.ylim(0, 1.2)
     plt.xlabel('Time (min)')
     plt.ylabel('Phosphorylated MEK\n(cytoplasm)')
-    plt.legend(loc='upper right',fontsize=12,frameon=False)
+    plt.legend(loc='upper right', fontsize=12, frameon=False)
 
-    plt.subplot(2,4,2)
-    plt.plot(sim.t,sim.PERK_cyt[:,0]/np.max(sim.PERK_cyt[:,1]),'b',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.subplot(2, 4, 2)  # ----------------------------------------------------
+    plt.plot(
+        sim.t, sim.PERK_cyt[:, sim.conditions.index('EGF')]/np.max(sim.PERK_cyt), 
+        'b', clip_on=False
     )
-    plt.plot(sim.t,sim.PERK_cyt[:,1]/np.max(sim.PERK_cyt[:,1]),'r',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.plot(
+        sim.t, sim.PERK_cyt[:, sim.conditions.index('HRG')]/np.max(sim.PERK_cyt), 
+        'r', clip_on=False
     )
-    plt.xlim(0,90)
-    plt.xticks([0,30,60,90])
-    plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
-    plt.ylim(0,1.2)
+    plt.xlim(0, 90)
+    plt.xticks([0, 30, 60, 90])
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2])
+    plt.ylim(0, 1.2)
     plt.xlabel('Time (min)')
     plt.ylabel('Phosphorylated ERK\n(cytoplasm)')
 
-    plt.subplot(2,4,3)
-    plt.plot(sim.t,sim.PRSK_wcl[:,0]/np.max(sim.PRSK_wcl[:,1]),'b',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.subplot(2, 4, 3)  # ----------------------------------------------------
+    plt.plot(
+        sim.t, sim.PRSK_wcl[:, sim.conditions.index('EGF')]/np.max(sim.PRSK_wcl), 
+        'b', clip_on=False
     )
-    plt.plot(sim.t,sim.PRSK_wcl[:,1]/np.max(sim.PRSK_wcl[:,1]),'r',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.plot(
+        sim.t, sim.PRSK_wcl[:, sim.conditions.index('HRG')]/np.max(sim.PRSK_wcl), 
+        'r', clip_on=False
     )
-    plt.xlim(0,90)
-    plt.xticks([0,30,60,90])
-    plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
-    plt.ylim(0,1.2)
+    plt.xlim(0, 90)
+    plt.xticks([0, 30, 60, 90])
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2])
+    plt.ylim(0, 1.2)
     plt.xlabel('Time (min)')
     plt.ylabel('Phosphorylated RSK\n(whole cell)')
 
-    plt.subplot(2,4,4)
-    plt.plot(sim.t,sim.PCREB_wcl[:,0]/np.max(sim.PCREB_wcl[:,1]),'b',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.subplot(2, 4, 4)  # ----------------------------------------------------
+    plt.plot(
+        sim.t, sim.PCREB_wcl[:, sim.conditions.index('EGF')]/np.max(sim.PCREB_wcl), 
+        'b', clip_on=False
     )
-    plt.plot(sim.t,sim.PCREB_wcl[:,1]/np.max(sim.PCREB_wcl[:,1]),'r',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.plot(
+        sim.t, sim.PCREB_wcl[:, sim.conditions.index('HRG')]/np.max(sim.PCREB_wcl), 
+        'r', clip_on=False
     )
-    plt.xlim(0,90)
-    plt.xticks([0,30,60,90])
-    plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
-    plt.ylim(0,1.2)
+    plt.xlim(0, 90)
+    plt.xticks([0, 30, 60, 90])
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2])
+    plt.ylim(0, 1.2)
     plt.xlabel('Time (min)')
     plt.ylabel('Phosphorylated CREB\n(whole cell)')
 
-    plt.subplot(2,4,5)
-    plt.plot(sim.t,sim.DUSPmRNA[:,0]/np.max(sim.DUSPmRNA[:,1]),'b',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.subplot(2, 4, 5)  # ----------------------------------------------------
+    plt.plot(
+        sim.t, sim.DUSPmRNA[:, sim.conditions.index('EGF')]/np.max(sim.DUSPmRNA), 
+        'b', clip_on=False
     )
-    plt.plot(sim.t,sim.DUSPmRNA[:,1]/np.max(sim.DUSPmRNA[:,1]),'r',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.plot(
+        sim.t, sim.DUSPmRNA[:, sim.conditions.index('HRG')]/np.max(sim.DUSPmRNA), 
+        'r', clip_on=False
     )
-    plt.xlim(0,90)
-    plt.xticks([0,30,60,90])
-    plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
-    plt.ylim(0,1.2)
+    plt.xlim(0, 90)
+    plt.xticks([0, 30, 60, 90])
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2])
+    plt.ylim(0, 1.2)
     plt.xlabel('Time (min)')
     plt.ylabel(r'$\it{dusp}$'+' mRNA\nexpression')
 
-    plt.subplot(2,4,6)
-    plt.plot(sim.t,sim.cFosmRNA[:,0]/np.max(sim.cFosmRNA[:,1]),'b',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.subplot(2, 4, 6)  # ----------------------------------------------------
+    plt.plot(
+        sim.t, sim.cFosmRNA[:, sim.conditions.index('EGF')]/np.max(sim.cFosmRNA), 
+        'b', clip_on=False
     )
-    plt.plot(sim.t,sim.cFosmRNA[:,1]/np.max(sim.cFosmRNA[:,1]),'r',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.plot(
+        sim.t, sim.cFosmRNA[:, sim.conditions.index('HRG')]/np.max(sim.cFosmRNA), 
+        'r', clip_on=False
     )
-    plt.xlim(0,90)
-    plt.xticks([0,30,60,90])
-    plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
-    plt.ylim(0,1.2)
+    plt.xlim(0, 90)
+    plt.xticks([0, 30, 60, 90])
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2])
+    plt.ylim(0, 1.2)
     plt.xlabel('Time (min)')
     plt.ylabel(r'$\it{c}$'+'-'+r'$\it{fos}$'+' mRNA\nexpression')
 
-    plt.subplot(2,4,7)
-    plt.plot(sim.t,sim.cFosPro[:,0]/np.max(sim.cFosPro[:,1]),'b',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.subplot(2, 4, 7)  # ----------------------------------------------------
+    plt.plot(
+        sim.t, sim.cFosPro[:, sim.conditions.index('EGF')]/np.max(sim.cFosPro), 
+        'b', clip_on=False
     )
-    plt.plot(sim.t,sim.cFosPro[:,1]/np.max(sim.cFosPro[:,1]),'r',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.plot(
+        sim.t, sim.cFosPro[:, sim.conditions.index('HRG')]/np.max(sim.cFosPro), 
+        'r', clip_on=False
     )
-    plt.xlim(0,90)
-    plt.xticks([0,30,60,90])
-    plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
-    plt.ylim(0,1.2)
+    plt.xlim(0, 90)
+    plt.xticks([0, 30, 60, 90])
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2])
+    plt.ylim(0, 1.2)
     plt.xlabel('Time (min)')
     plt.ylabel('c-Fos Protein\nexpression')
 
-    plt.subplot(2,4,8)
-    plt.plot(sim.t,sim.PcFos[:,0]/np.max(sim.PcFos[:,1]),'b',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.subplot(2, 4, 8)  # ----------------------------------------------------
+    plt.plot(
+        sim.t, sim.PcFos[:, sim.conditions.index('EGF')]/np.max(sim.PcFos), 
+        'b', clip_on=False
     )
-    plt.plot(sim.t,sim.PcFos[:,1]/np.max(sim.PcFos[:,1]),'r',
-        path_effects=[path_effects.SimpleLineShadow(),
-        path_effects.Normal()],clip_on=False
+    plt.plot(
+        sim.t, sim.PcFos[:, sim.conditions.index('HRG')]/np.max(sim.PcFos), 
+        'r', clip_on=False
     )
-    plt.xlim(0,90)
-    plt.xticks([0,30,60,90])
-    plt.yticks([0,0.2,0.4,0.6,0.8,1,1.2])
-    plt.ylim(0,1.2)
+    plt.xlim(0, 90)
+    plt.xticks([0, 30, 60, 90])
+    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1, 1.2])
+    plt.ylim(0, 1.2)
     plt.xlabel('Time (min)')
     plt.ylabel('Phosphorylated c-Fos\nProtein expression')
 
-    plt.savefig('cFos_model.png',bbox_inches='tight')
+    plt.savefig('cFos_model.png', bbox_inches='tight')
